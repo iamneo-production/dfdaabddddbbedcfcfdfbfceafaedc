cb7638acd1317be48780999d689c090905cf826e
Iamneo <techadmin@iamneo.ai>
2023-08-08 07:41:37 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,13 +1,45 @@
-package com.examly.springapp;
+import com.examly.springapp.entity.Taskentity;
+import com.examly.springapp.service.TaskService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+@RestController
+@RequestMapping()
+public class TaskController {
+    @Autowired
+    private TaskService service;
+    
+    @PostMapping("/saveTask")
+    public Taskentity saveTask(@RequestBody Taskentity taskentity){
+        Taskentity output = service.saveTask(taskentity);
+        return output;
+    }
 
-@SpringBootApplication
-public class SpringappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
-	}
+    @GetMapping("/changeStatus")
+    private Taskentity updatetaskStatus(@RequestParam("id") String id){
+        return service.updatetaskStatus(id);
+    }
+
+    @GetMapping("/deleteTask")
+    private String deleteTask(@RequestParam("id") String id){
+        return service.deleteTask(id);
+    }
+    @GetMapping("/alltasks")
+    private List<Taskentity> getallTasks(){
+        return service.getallTasks();
+    }
+
+    @GetMapping("/getTask")
+    private Taskentity getTask(@RequestParam("id") String id){
+        return service.gettaskbyid(id);
+    }
 
 }
